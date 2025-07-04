1bddca283ac380a9ddc7dbf7910186073f421933
Kamepalli Narendra <121089941+Nari-2002@users.noreply.github.com>
2025-07-04 21:31:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain.chains import LLMChain
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
